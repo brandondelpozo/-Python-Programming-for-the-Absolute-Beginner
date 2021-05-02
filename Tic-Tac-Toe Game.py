5ec715284dfc2b9ceffd1c195fe1d705ab238691
branzel <branzzel@gmail.com>
2021-05-01 23:08:38 -0500

    Update Tic-Tac-Toe Game.py

--- Tic-Tac-Toe Game.py
+++ Tic-Tac-Toe Game.py
@@ -125,6 +125,106 @@ def human_move(board, human):
     print("Fine...")
     return move
 
+def computer_move(board, computer, human):
+    """Make computer move."""
+    # make a copy to work with since function will be changing list
+    board = board[:]
+    # the best positions to have, in order
+    BEST_MOVES = (4, 0, 2, 6, 8, 1, 3, 5, 7)
+    print("I shall take square number", end=" ")
+
+# if computer can win, take that mave
+for move in legal_moves(board):
+    board[move] = computer
+    if winner(board) == computer:
+        print(move)
+        return move
+    # done checking this move, unde it
+    board[move] = EMPTY
+
+# if human can win, block that move
+for move in legal_moves(board):
+    board[move] = human
+    if winner(board) == human:
+        print(move)
+        return move
+    # done checking this move, undo it
+    board[move] = EMPTY
+
+# since no one can win on next move, pick best open square
+for move in BEST_MOVES:
+    if move in legal_moves(board):
+        print(move)
+        return move
+
+def next_turn(turn):
+    """Switch turns."""
+    if turn == X:
+        return 0
+
+def congrat_winner(the_winner, computer, human):
+    """Congratulate the winner."""
+    if the_winner != TIE:
+        print(the_winner, "won!\n")
+    else:
+        print("It's a tie\n")
+
+    if the_winner == computer:
+        print("As I predicted, human, I am triumphant once more. \n" \
+              "Prood that computers are superior to humans in all regards.")
+    elif the_winner == human:
+        print("No, no! It cannot be! Somehow you tricked me, human. \n" \
+              "But never again! I, the computer, so swear it!")
+    elif the_winner == TIE:
+        print("You were most lucky, human, and somehow managed to tie me. \n" \
+              "Celebrate today... for this is the best you will ever achieve.")
+
+def main():
+    display_instruct()
+    computer, human = pieces()
+    turn = X
+    board = new_broad()
+    display_board(board)
+
+    while not winner(board):
+        if turn == human:
+            move = human_move(board, human)
+            board[move] = human
+        else:
+            move = computer_move(board, computer, human)
+            board[move] = computer
+        display_board(board)
+        turn = next_turn(turn)
+
+    the_winner = winner(board)
+    congrat_winner(the_winner, computer, human)
+
+# start the program
+main()
+input("\n\nPress the enter key to quit.")
+    
+            
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+     
+
+
+
 
 
 
