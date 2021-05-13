9fe54b033d736feff40cc8f2ed5d26de9988c47e
branzel <branzzel@gmail.com>
2021-05-12 23:22:33 -0500

    Update Guess My Number improved with ask number function_USING GLOBAL BUILT IN.py

--- Guess My Number improved with ask number function_USING GLOBAL BUILT IN.py
+++ Guess My Number improved with ask number function_USING GLOBAL BUILT IN.py
@@ -1,6 +1,6 @@
 # Guess My Number
 #
-# The computer picks a random number between 1 and 100
+# The computer picks a random number between 1 and 10
 # The player tries to guess it and the computer lets
 # the player know if the guess is too high, too low
 # or right on the money
@@ -13,7 +13,6 @@ print("Try to guess it in as few attemps as possible.\n")
 #set the initial values
 the_number = random.randint(1, 10)
 tries = 1
-print(the_number)
 
 # ask number
 def ask_number(question, low, high):
@@ -23,31 +22,24 @@ def ask_number(question, low, high):
         response = int(input(question))
     return response
 
-#def tries():
-#    tries += 1
-#    return tries
-
 # guessing loop
 def main():
-    #tries = 1
     guess = int(ask_number("Take a guess: ", 1, 10))    
     while guess != the_number:
         if guess > the_number:
             print("Lower...")
-        else:
+        elif guess < the_number:
             print("Higher...")
+        else:
+            print("Your guess is outside the range")
 
         guess = int(ask_number("Take a guess: ", 1, 10))
         global tries
         tries += 1
     return guess, tries
 
-#number of tries
-
-
 #start the program
 main()
-
 
     
 print("You guessed it! The number was", the_number)
