53128910bf629531c68d7845cbad1583a3ff7df1
branzel <branzzel@gmail.com>
2021-06-13 03:06:34 -0500

    critter_caretaker.py finished

--- property_critter2.py
+++ property_critter2.py
@@ -3,9 +3,10 @@
 
 class Critter(object):
     """A virtual pet"""
-    def __init__(self, name):
+    def __init__(self, name, age):
         print("A new critter has been born!")
         self.__name = name   #this is a private attribute
+        self.__age = age
 
     @property               #I use property here because I wanto to makes changes to the private attribute
     def name(self):
@@ -20,8 +21,37 @@ class Critter(object):
             print("Name change successful.")
 
     def talk(self):
-        print("\nHi, I'm", self.name)
+        print("\nHi, I'm", self.name, "I'm", self.age)
 
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, new_age):
+        if new_age == "":
+            print("A critter's age can't be empty string")
+        else:
+            self.__age = new_age
+            print("Age change successful.")
+
+# main
+crit = Critter("Jhony", 21)
+crit.talk()
+
+print("\nAttempting to change my critter's name to PocaPoca...")
+crit.name = "PocaPoca"
+
+print("My critter's name is:", end=" ")
+print(crit.name)
+
+
+print("\nAttempting to change my critter's age to 24...")
+crit.age = 24
+
+crit.talk()
+
+"""
 # main
 crit = Critter("Poochie")
 crit.talk()
@@ -40,6 +70,6 @@ crit.name = ""
 
 print("My critter's name is:", end=" ")
 print(crit.name)
-
+"""
 
 input("\n\nPress enter key to exit")
