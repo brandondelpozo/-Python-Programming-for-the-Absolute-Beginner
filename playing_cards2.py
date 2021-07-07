cb5c4f2a62d7e25082788998d17093aa2c574a39
brandondelpozo <branzzel@gmail.com>
2021-07-07 00:22:33 -0500

    update playin_cards2

--- playing_cards2.py
+++ playing_cards2.py
@@ -1,1 +1,42 @@
-playing_cards2 = ""
+# Playing Cards 2.0
+# Demonstrates inheritance - class extension
+
+from typing import ClassVar
+
+
+class Card(object):
+    """" A playing card. """
+    RANK = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    SUITS = ["c", "d", "h", "s"]
+
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+    
+    def __str__(self):
+        rep = self.rank + self.suit
+        return rep
+
+class Hand(object):
+    """A hand of paying cards."""
+    def __init__(self):
+        self.cards = []
+
+    def __str__(self):
+        if self.cards:
+            rep = ""
+            for card in self.cards:
+                rep += str(card) + "\t"
+        else:
+            rep = "<empty>"
+        return rep
+    
+    def clear(self):
+        self.cards = []
+
+    def add(self, card):
+        self.cards.append(card)
+
+    def give(self, card, other_hand):
+        self.cards.remove(card)
+        other_hand.add(card)
