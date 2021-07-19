2f2897ad255c1605732b1df88882fdb75fa78b32
brandondelpozo <branzzel@gmail.com>
2021-07-18 23:57:11 -0500

    update playing_cards2.py

--- playing_cards2.py
+++ playing_cards2.py
@@ -2,7 +2,7 @@
 # Demonstrates inheritance - class extension
 
 class Card(object):
-    """" A playing card. """
+    """"A playing card."""
     RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     SUITS = ["c", "d", "h", "s"]
 
