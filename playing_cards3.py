bf781d40589f506f09b268364fe492026eb0cd37
brandondelpozo <branzzel@gmail.com>
2021-07-19 23:26:36 -0500

    update playing_cards3.py

---
+++ playing_cards3.py
@@ -0,0 +1,17 @@
+# Playing Cards 3.0
+# Demonstrates inheritance - overriding methods
+
+class Card(object):
+    """ A playing card. """
+    RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    SUITS = ["c", "d", "h", "s"]
+
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __str__(self):
+        rep = self.rank + self.suit
+        return rep
+
+class Unprintable_Card(Card):
